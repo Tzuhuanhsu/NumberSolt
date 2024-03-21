694c578545c8c512cbc8790c04ea086a093b9fed
Tzuhuanhsu <lv7250404@yahoo.com.tw>
2024-03-21 15:47:56 +0800

    新增拓展插件
    產出 bundle-version 的檔案

---
+++ extensions/buildhelper/dist/builder.js
@@ -0,0 +1,130 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.assetHandlers = exports.configs = exports.unload = exports.load = void 0;
+const load = function () {
+    console.debug(`${PACKAGE_NAME} load`);
+};
+exports.load = load;
+const unload = function () {
+    console.debug(`${PACKAGE_NAME} unload`);
+};
+exports.unload = unload;
+const PACKAGE_NAME = 'buildhelper';
+const complexTestItems = {
+    number: {
+        label: `i18n:${PACKAGE_NAME}.options.complexTestNumber`,
+        description: `i18n:${PACKAGE_NAME}.options.complexTestNumber`,
+        default: 80,
+        render: {
+            ui: 'ui-num-input',
+            attributes: {
+                step: 1,
+                min: 0,
+            },
+        },
+    },
+    string: {
+        label: `i18n:${PACKAGE_NAME}.options.complexTestString`,
+        description: `i18n:${PACKAGE_NAME}.options.complexTestString`,
+        default: 'cocos',
+        render: {
+            ui: 'ui-input',
+            attributes: {
+                placeholder: `i18n:${PACKAGE_NAME}.options.enterCocos`,
+            },
+        },
+        verifyRules: ['ruleTest'],
+    },
+    boolean: {
+        label: `i18n:${PACKAGE_NAME}.options.complexTestBoolean`,
+        description: `i18n:${PACKAGE_NAME}.options.complexTestBoolean`,
+        default: true,
+        render: {
+            ui: 'ui-checkbox',
+        },
+    },
+};
+exports.configs = {
+    '*': {
+        hooks: './hooks',
+        doc: 'editor/publish/custom-build-plugin.html',
+        options: {
+            remoteAddress: {
+                label: `i18n:${PACKAGE_NAME}.options.remoteAddress`,
+                default: 'https://www.cocos.com/',
+                render: {
+                    ui: 'ui-input',
+                    attributes: {
+                        placeholder: 'Enter remote address...',
+                    },
+                },
+                verifyRules: ['required'],
+            },
+            enterCocos: {
+                label: `i18n:${PACKAGE_NAME}.options.enterCocos`,
+                description: `i18n:${PACKAGE_NAME}.options.enterCocos`,
+                default: '',
+                render: {
+                    /**
+                     * @en Please refer to Developer -> UI Component for a list of all supported UI components
+                     * @zh 请参考 开发者 -> UI 组件 查看所有支持的 UI 组件列表
+                     */
+                    ui: 'ui-input',
+                    attributes: {
+                        placeholder: `i18n:${PACKAGE_NAME}.options.enterCocos`,
+                    },
+                },
+                verifyRules: ['ruleTest'],
+                verifyLevel: 'warn',
+            },
+            selectTest: {
+                label: `i18n:${PACKAGE_NAME}.options.selectTest`,
+                description: `i18n:${PACKAGE_NAME}.options.selectTest`,
+                default: 'option2',
+                render: {
+                    ui: 'ui-select',
+                    items: [
+                        {
+                            label: `i18n:${PACKAGE_NAME}.options.selectTestOption1`,
+                            value: 'option1',
+                        },
+                        {
+                            label: `i18n:${PACKAGE_NAME}.options.selectTestOption2`,
+                            value: 'option2',
+                        },
+                    ],
+                },
+            },
+            objectTest: {
+                label: `i18n:${PACKAGE_NAME}.options.objectTest`,
+                description: `i18n:${PACKAGE_NAME}.options.objectTest`,
+                type: 'object',
+                default: {
+                    number: complexTestItems.number.default,
+                    string: complexTestItems.string.default,
+                    boolean: complexTestItems.boolean.default,
+                },
+                itemConfigs: complexTestItems,
+            },
+            arrayTest: {
+                label: `i18n:${PACKAGE_NAME}.options.arrayTest`,
+                description: `i18n:${PACKAGE_NAME}.options.arrayTest`,
+                type: 'array',
+                default: [complexTestItems.number.default, complexTestItems.string.default, complexTestItems.boolean.default],
+                itemConfigs: JSON.parse(JSON.stringify(Object.values(complexTestItems))),
+            },
+        },
+        verifyRuleMap: {
+            ruleTest: {
+                message: `i18n:${PACKAGE_NAME}.options.ruleTest_msg`,
+                func(val, buildOptions) {
+                    if (val === 'cocos') {
+                        return true;
+                    }
+                    return false;
+                },
+            },
+        },
+    },
+};
+exports.assetHandlers = './asset-handlers';
---
+++ extensions/buildhelper/dist/hooks.js
@@ -0,0 +1,91 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.onAfterMake = exports.onBeforeMake = exports.onError = exports.unload = exports.onAfterBuild = exports.onAfterCompressSettings = exports.onBeforeCompressSettings = exports.onBeforeBuild = exports.load = exports.throwError = void 0;
+const PACKAGE_NAME = 'buildhelper';
+function log(...arg) {
+    return console.log(`[${PACKAGE_NAME}] `, ...arg);
+}
+let allAssets = [];
+exports.throwError = true;
+const load = function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`[${PACKAGE_NAME}] Load cocos plugin example in builder.`);
+        allAssets = yield Editor.Message.request('asset-db', 'query-assets');
+    });
+};
+exports.load = load;
+const onBeforeBuild = function (options, result) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Todo some thing
+        log(`${PACKAGE_NAME}.webTestOption`, 'onBeforeBuild');
+    });
+};
+exports.onBeforeBuild = onBeforeBuild;
+const onBeforeCompressSettings = function (options, result) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const pkgOptions = options.packages[PACKAGE_NAME];
+        // Todo some thing
+        console.debug('get settings test', result.settings);
+    });
+};
+exports.onBeforeCompressSettings = onBeforeCompressSettings;
+const onAfterCompressSettings = function (options, result) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Todo some thing
+        console.log('webTestOption', 'onAfterCompressSettings');
+    });
+};
+exports.onAfterCompressSettings = onAfterCompressSettings;
+const onAfterBuild = function (options, result) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("onAfterBuild", result.paths.settings);
+        // change the uuid to test
+        const uuidTestMap = {
+            image: '57520716-48c8-4a19-8acf-41c9f8777fb0',
+        };
+        for (const name of Object.keys(uuidTestMap)) {
+            const uuid = uuidTestMap[name];
+            console.debug(`containsAsset of ${name}`, result.containsAsset(uuid));
+            console.debug(`getAssetPathInfo of ${name}`, result.getAssetPathInfo(uuid));
+            console.debug(`getRawAssetPaths of ${name}`, result.getRawAssetPaths(uuid));
+            console.debug(`getJsonPathInfo of ${name}`, result.getJsonPathInfo(uuid));
+        }
+        // test onError hook
+        // throw new Error('Test onError');
+    });
+};
+exports.onAfterBuild = onAfterBuild;
+const unload = function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`[${PACKAGE_NAME}] Unload cocos plugin example in builder.`);
+    });
+};
+exports.unload = unload;
+const onError = function (options, result) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Todo some thing
+        console.warn(`${PACKAGE_NAME} run onError`);
+    });
+};
+exports.onError = onError;
+const onBeforeMake = function (root, options) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`onBeforeMake: root: ${root}, options: ${options}`);
+    });
+};
+exports.onBeforeMake = onBeforeMake;
+const onAfterMake = function (root, options) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`onAfterMake: root: ${root}, options: ${options}`);
+    });
+};
+exports.onAfterMake = onAfterMake;
